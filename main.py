b270b3b19d2b33addd379c38fcb6505667cc84f4
Vick Javariz <66493562+javavick@users.noreply.github.com>
2025-03-02 15:19:21 -0500

    Make certain asteroids split when shot

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def main():
             
             for shot in shots:
                 if shot.collision(asteroid):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
         
         pygame.display.flip()
